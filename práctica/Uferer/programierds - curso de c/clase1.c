bfecd0bd154646b1a055da825c36a4c0617cba95
delacortejm <delacortejm@gmail.com>
2026-04-05 18:04:12 -0300

    Refactor code structure for improved readability and maintainability

---
+++ práctica/Uferer/programierds - curso de c/clase1.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // Directiva del preprocesador
+
+// Función principal: punto de entrada del programa
+int main() {
+    printf("Hola, Mundo!"); // Instrucción para mostrar texto
+    return 0; // Código de salida (0 significa éxito)
+}
