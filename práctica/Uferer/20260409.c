f26f6ca84c7fe9b746fb581ee4becf1343e5a45f
delacortejm <delacortejm@gmail.com>
2026-04-09 21:53:36 -0300

    Refactor main function to compute squares of an array and comment out unused code

--- práctica/Uferer/20260409.c
+++ práctica/Uferer/20260409.c
@@ -1,22 +1,40 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
-{
-    char palabra[] = "computadora";
-    char *ptr = strchr(palabra, 'u');
+// int main()
+// {
+//     char palabra[] = "computadora";
+//     char *ptr = strchr(palabra, 'u');
     
-    printf("La palabra es: %s\n", palabra);
-    printf("PTR es: %p\n", (void*)ptr);
+//     printf("La palabra es: %s\n", palabra);
+//     printf("PTR es: %p\n", (void*)ptr);
 
-    if (ptr != NULL)
-    {
-        printf("Encontrado: %c en la posicion %ld\n", *ptr, ptr - palabra);
+//     if (ptr != NULL)
+//     {
+//         printf("Encontrado: %c en la posicion %ld\n", *ptr, ptr - palabra);
+//     }
+//     else
+//     {
+//         printf("No se encontró el carácter\n");
+//     }
+
+//     return 0;
+// }
+
+//Quiero que me muestren un nuevo array de números que sea el cuadrado de cada elemento de este array {3, 2, 1,5,2,9,2}
+
+int main() {
+    int numeros[] = {3, 2, 1, 5, 2, 9, 2};
+    int cuadrados[7];
+
+    for (int i = 0; i < 7; i++) {
+        cuadrados[i] = numeros[i] * numeros[i];
     }
-    else
-    {
-        printf("No se encontró el carácter\n");
+
+    for (int i = 0; i < 7; i++) {
+        printf("%d ", cuadrados[i]);
     }
+    printf("\n");
 
     return 0;
 }
