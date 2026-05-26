3aa7b5584ec5844c70c6dd2d641e955a331ce9b9
delacortejm <delacortejm@gmail.com>
2026-05-25 21:43:30 -0300

    preparación 2do parcial

--- práctica/Uferer/programierds - curso de c/clase6.c
+++
@@ -1,9 +0,0 @@
-/* Estructuras de control: if, if-else, if anidados y switch
-
-Comparación entre if y switch
-
-Estructura	Cuándo usarla
-    if	Cuando se comparan condiciones complejas
-    if - else	Cuando hay dos posibles resultados
-    if anidados	Cuando hay varias decisiones dependientes
-    switch	Cuando se comparan muchos valores específicos*/
--- práctica/tp 3/3_6_4.c
+++
@@ -1,9 +0,0 @@
-do {
-    nomina = nomina + sal;
-    i++;
-    printf("Ingrese el salario del profesor, 0 para terminar: ");
-    scanf("%f", &sal);
-} while (sal != 0);
-
-prom = nomina / i;
-printf("\nNomina: %.2f \t Promedio de salarios: %.2f\n", nomina, prom);
